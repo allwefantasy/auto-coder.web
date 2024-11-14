fd84acb322afdde83d3d6f6cf5aa26a15522063b
WilliamZhu <allwefantasy@gmail.com>
2024-11-14 09:25:34 +0800

    Revert "auto_coder_103_chat_action.yml_8893621850a6c7ffb8e1dff49d2f0e45"
    This reverts commit c3878a06b9e70e96f1ee1e043d44c061a6616950.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
-import FileList from './components/FileList';
-import GroupList from './components/GroupList';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col h-screen">
-        <Header />
-        <div className="flex flex-1 overflow-hidden">
-          <Sidebar />
-          <main className="flex-1 overflow-y-auto bg-gray-100">
-            <Routes>
-              <Route path="/" element={<FileList />} />
-              <Route path="/groups" element={<GroupList />} />
-            </Routes>
-          </main>
-        </div>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
