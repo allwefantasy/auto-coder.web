0799ffd7b0e2ffd4134ebc760a6577789afafe4d
WilliamZhu <allwefantasy@gmail.com>
2024-11-17 09:33:09 +0800

    auto_coder_pre_162_chat_action.yml_2cec4348999bb599efe8ac733f33af2c

--- frontend/src/components/Sidebar/ChatPanel.tsx
+++ frontend/src/components/Sidebar/ChatPanel.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { AutoComplete, Card, Select, Switch, message, Tooltip } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 
