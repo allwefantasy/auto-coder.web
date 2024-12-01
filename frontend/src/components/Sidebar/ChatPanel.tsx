81078edb9802829d1d2a3f4771e74d914a0c7d41
WilliamZhu <allwefantasy@gmail.com>
2024-12-01 21:34:40 +0800

    auto_coder_pre_362_chat_action.yml_bb5b29c25a368b5c0083bf5239d8c75a

--- frontend/src/components/Sidebar/ChatPanel.tsx
+++ frontend/src/components/Sidebar/ChatPanel.tsx
@@ -856,7 +856,7 @@ const ChatPanel: React.FC<ChatPanelProps> = ({ setPreviewFiles, setRequestId, se
           className="absolute top-2 left-2 z-10 p-1.5 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg transition-colors"
           title="New Chat"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-2 w-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
           </svg>
         </button>
