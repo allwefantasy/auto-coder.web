c0cce7888024987906d46f7af6ba116ab3bac815
WilliamZhu <allwefantasy@gmail.com>
2025-02-28 23:55:54 +0800

    auto

--- docker/build-with-retry.sh
+++ docker/build-with-retry.sh
@@ -154,6 +154,7 @@ if [[ "$USE_CLEAN" == "false" ]]; then
     BUILD_OPTS="$BUILD_OPTS --no-clean"
     print_yellow "将跳过清理步骤"
 else
+    BUILD_OPTS="$BUILD_OPTS --clean"
     print_yellow "将在构建前执行清理"
 fi
 
