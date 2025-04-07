1cb4d057dcffd64723b0f2bb0edb9df9cdaaa459
WilliamZhu <allwefantasy@gmail.com>
2025-04-07 22:39:44 +0700

    下面是我们已经产生的一个消息列表,其中 USER_RESPONSE 表示用户的输入，RESULT 你的输出： <messages>
    <message>
    <type>USER_RESPONSE</type>
    <content><RagSelector /> 和<MCPsSelector />   <CodeModelSelector /> 三者要横向排列</content>
    </message>
    
    </messages>
    
    下面是用户的最新需求：
    <request>
    <RagSelector /> 和<MCPsSelector />   <CodeModelSelector /> 三者要横向排列    
    </request>
    auto_coder_000000000404_chat_action.yml

--- frontend/src/components/Sidebar/ProviderSelectors.tsx
+++ frontend/src/components/Sidebar/ProviderSelectors.tsx
@@ -9,26 +9,23 @@ interface ProviderSelectorsProps {
 
 const ProviderSelectors: React.FC<ProviderSelectorsProps> = ({ isWriteMode }) => {
   return (
-    <div className="w-full space-y-2"> {/* Add vertical spacing */}
-      {/* Conditionally render RAG based on write mode */}
-      {!isWriteMode && (
-          <RagSelector />
-      )}
+    <div className="w-full"> 
+      <div className="flex space-x-2 w-full"> 
+        {/* Conditionally render RAG based on write mode */}
+        {!isWriteMode && (
+            <div className="flex-1">
+              <RagSelector />
+            </div>
+        )}
 
-      {/* Always render MCPs and Code Model selectors (or adjust based on requirements) */}
-      {/* Use flex layout for MCPs and Code Model if they should be side-by-side */}
-      {/* Or stack them vertically */}
-      
-      {/* Example: Stacking them vertically */}
-      <MCPsSelector />
-      <CodeModelSelector />
-
-      {/* Example: Placing them side-by-side (adjust flex properties as needed) */}
-      {/* <div className="flex space-x-2 w-full">
-        <div className="flex-1"> <MCPsSelector /> </div>
-        <div className="flex-1"> <CodeModelSelector /> </div>
-      </div> */}
-      
+        {/* Always render MCPs and Code Model selectors */}
+        <div className="flex-1">
+          <MCPsSelector />
+        </div>
+        <div className="flex-1">
+          <CodeModelSelector />
+        </div>
+      </div>
     </div>
   );
 };
